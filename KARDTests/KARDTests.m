a99a6baa5c4f064d07557dba19bbc75c8f1292b7
DisruptiveMind <tyler@disruptiveart.com>
2013-02-20 17:42:01 -0500

    Added KARDTests

---
+++ KARDTests/KARDTests.m
@@ -0,0 +1,32 @@
+//
+//  KARDTests.m
+//  KARDTests
+//
+//  Created by Tyler on 2013-02-19.
+//  Copyright (c) 2013 ICAD. All rights reserved.
+//
+
+#import "KARDTests.h"
+
+@implementation KARDTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in KARDTests");
+}
+
+@end
